9336c848fd66269b8eaa22bce982be05406f2d5f
Daniel Santos <danielsantos7@outlook.com.br>
2026-06-09 01:09:12 -0300

    Refactor cosine address calculation in sine_cos_lut; simplify pointer logic and remove unused signal

--- rtl/sine_cos_lut.vhd
+++ rtl/sine_cos_lut.vhd
@@ -15,8 +15,6 @@ end entity sine_cos_lut;
 
 architecture rtl of sine_cos_lut is
 
-    signal cos_adr : unsigned(LUT_ADDR_BITS+1 downto 0);
-
     signal sin_pointer : std_logic_vector(LUT_ADDR_BITS-1 downto 0);
     signal cos_pointer : std_logic_vector(LUT_ADDR_BITS-1 downto 0);
 
@@ -31,16 +29,14 @@ architecture rtl of sine_cos_lut is
 
 begin
 
-    cos_adr <= unsigned(adr_i) + to_unsigned(2**LUT_ADDR_BITS, LUT_ADDR_BITS+2);
-
     sin_pointer <= not adr_i(LUT_ADDR_BITS-1 downto 0) when adr_i(LUT_ADDR_BITS) = '1' else adr_i(LUT_ADDR_BITS-1 downto 0);
-    cos_pointer <= not std_logic_vector(cos_adr(LUT_ADDR_BITS-1 downto 0)) when cos_adr(LUT_ADDR_BITS) = '1' else std_logic_vector(cos_adr(LUT_ADDR_BITS-1 downto 0));
+    cos_pointer <= not adr_i(LUT_ADDR_BITS-1 downto 0) when adr_i(LUT_ADDR_BITS) = '0' else adr_i(LUT_ADDR_BITS-1 downto 0);
 
     sin_val <= SINE_TABLE(to_integer(unsigned(sin_pointer)));
     cos_val <= SINE_TABLE(to_integer(unsigned(cos_pointer)));
 
     sin_mux <= std_logic_vector(unsigned(not sin_val) + 1) when adr_i(LUT_ADDR_BITS + 1) = '1' else sin_val;
-    cos_mux <= std_logic_vector(unsigned(not cos_val) + 1) when cos_adr(LUT_ADDR_BITS + 1) = '1' else cos_val;
+    cos_mux <= std_logic_vector(unsigned(not cos_val) + 1) when (adr_i(LUT_ADDR_BITS + 1) xor adr_i(LUT_ADDR_BITS)) = '1' else cos_val;
 
     reg_proc : process(rst_i, clk_i)
     begin
