7fd8cf0fa41d68ee84ff1896b1637d066d27d80a
Daniel Santos <danielsantos7@outlook.com.br>
2026-06-10 00:27:03 -0300

    Refactor sine_cos_lut to use dedicated signals for sine and cosine negation; improve clarity in mux logic

--- rtl/sine_cos_lut.vhd
+++ rtl/sine_cos_lut.vhd
@@ -17,6 +17,9 @@ architecture rtl of sine_cos_lut is
 
     signal sin_pointer : std_logic_vector(LUT_ADDR_BITS-1 downto 0);
     signal cos_pointer : std_logic_vector(LUT_ADDR_BITS-1 downto 0);
+    
+    signal sin_neg : std_logic;
+    signal cos_neg : std_logic;
 
     signal sin_val : std_logic_vector(OUT_RES_BITS-1 downto 0);
     signal cos_val : std_logic_vector(OUT_RES_BITS-1 downto 0);
@@ -32,11 +35,14 @@ begin
     sin_pointer <= not adr_i(LUT_ADDR_BITS-1 downto 0) when adr_i(LUT_ADDR_BITS) = '1' else adr_i(LUT_ADDR_BITS-1 downto 0);
     cos_pointer <= not adr_i(LUT_ADDR_BITS-1 downto 0) when adr_i(LUT_ADDR_BITS) = '0' else adr_i(LUT_ADDR_BITS-1 downto 0);
 
+    sin_neg <= adr_i(LUT_ADDR_BITS + 1);
+    cos_neg <= adr_i(LUT_ADDR_BITS + 1) xor adr_i(LUT_ADDR_BITS);
+
     sin_val <= SINE_TABLE(to_integer(unsigned(sin_pointer)));
     cos_val <= SINE_TABLE(to_integer(unsigned(cos_pointer)));
 
-    sin_mux <= std_logic_vector(unsigned(not sin_val) + 1) when adr_i(LUT_ADDR_BITS + 1) = '1' else sin_val;
-    cos_mux <= std_logic_vector(unsigned(not cos_val) + 1) when (adr_i(LUT_ADDR_BITS + 1) xor adr_i(LUT_ADDR_BITS)) = '1' else cos_val;
+    sin_mux <= std_logic_vector(unsigned(not sin_val) + 1) when sin_neg = '1' else sin_val;
+    cos_mux <= std_logic_vector(unsigned(not cos_val) + 1) when cos_neg = '1' else cos_val;
 
     reg_proc : process(rst_i, clk_i)
     begin
