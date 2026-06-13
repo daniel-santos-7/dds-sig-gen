91377a8069147a11f49b2be770e5c78c987e28fc
Daniel Santos <danielsantos7@outlook.com.br>
2026-06-12 22:53:27 -0300

    Refactor env_seq component; improve signal handling and rename signals for clarity and consistency.

--- rtl/env_seq.vhd
+++ rtl/env_seq.vhd
@@ -5,75 +5,73 @@ use work.envelope_lut_pkg.ENV_LUT_ADDR_BITS;
 
 entity env_seq is
     port (
-        clk_i        : in  std_logic;
-        rst_i        : in  std_logic;
-        valid_i      : in  std_logic;
-        delay_i      : in  std_logic_vector(23 downto 0);
-        step_i       : in  std_logic_vector(31 downto 0);
-        sync_o    : out std_logic;
+        clk_i    : in  std_logic;
+        rst_i    : in  std_logic;
+        valid_i  : in  std_logic;
+        delay_i  : in  std_logic_vector(23 downto 0);
+        step_i   : in  std_logic_vector(31 downto 0);
+        sync_o   : out std_logic;
         addr_o   : out std_logic_vector(ENV_LUT_ADDR_BITS-1 downto 0);
         active_o : out std_logic;
-        ready_o      : out std_logic
+        ready_o  : out std_logic
     );
 end entity env_seq;
 
 architecture rtl of env_seq is
 
-    signal ready      : std_logic;
-    signal env_cnt    : unsigned(32 downto 0);
-    signal delay_cnt  : unsigned(23 downto 0);
-    signal env_active : std_logic;
-    signal env_done   : std_logic;
-    signal delay_done : std_logic;
+    signal ready : std_logic;
+    
+    signal env_cnt_inc  : unsigned(32 downto 0);
+    signal env_cnt_val  : unsigned(32 downto 0);
+    signal env_cnt_reg  : unsigned(32 downto 0);
+    signal env_cnt_done : std_logic;
+    
+    signal delay_cnt_reg  : unsigned(23 downto 0);
+    signal delay_cnt_done : std_logic;
 
 begin
 
-    env_active_proc : process(clk_i)
+    env_cnt_inc <= unsigned('0' & step_i);
+    env_cnt_val <= env_cnt_reg + env_cnt_inc;
+
+    env_cnt_reg_proc : process(clk_i)
     begin
         if rising_edge(clk_i) then
             if rst_i = '1' then
-                env_active <= '0';
+                env_cnt_reg <= (32 => '1', others => '0');
             elsif valid_i = '1' and ready = '1' then
-                env_active <= '1';
-            elsif env_done = '1' then
-                env_active <= '0';
+                env_cnt_reg <= (others => '0');
+            elsif env_cnt_done = '0' then
+                if env_cnt_val(32) = '1' then
+                    env_cnt_reg <= (32 => env_cnt_val(32), others => '0');
+                else
+                    env_cnt_reg <= env_cnt_val;
+                end if;
             end if;
         end if;
-    end process env_active_proc;
+    end process env_cnt_reg_proc;
 
-    env_cnt_proc : process(clk_i)
+    env_cnt_done <= env_cnt_reg(32);
+
+    delay_cnt_reg_proc : process(clk_i)
     begin
         if rising_edge(clk_i) then
             if rst_i = '1' then
-                env_cnt <= (32 => '1', others => '0');
+                delay_cnt_reg <= (others => '0');
             elsif valid_i = '1' and ready = '1' then
-                env_cnt <= (others => '0');
-            elsif env_active = '1' and env_done = '0' then
-                env_cnt <= env_cnt + unsigned('0' & step_i);
+                delay_cnt_reg <= unsigned(delay_i);
+            elsif env_cnt_done = '1' and delay_cnt_done = '0' then
+                delay_cnt_reg <= delay_cnt_reg - 1;
             end if;
         end if;
-    end process env_cnt_proc;
+    end process delay_cnt_reg_proc;
 
-    delay_proc : process(clk_i)
-    begin
-        if rising_edge(clk_i) then
-            if rst_i = '1' then
-                delay_cnt <= (others => '0');
-            elsif valid_i = '1' and ready = '1' then
-                delay_cnt <= unsigned(delay_i);
-            elsif env_done = '1' and delay_done = '0' then
-                delay_cnt <= delay_cnt - 1;
-            end if;
-        end if;
-    end process delay_proc;
-
-    env_done    <= env_cnt(32);
-    delay_done  <= '1' when delay_cnt = 0 else '0';
-    ready       <= '1' when env_active = '0' and delay_done = '1' else '0';
-
-    ready_o      <= ready;
-    sync_o    <= valid_i and ready;
-    addr_o   <= std_logic_vector(env_cnt(31 downto 32-ENV_LUT_ADDR_BITS)) when env_active = '1' else (others => '0');
-    active_o <= env_active and not env_done;
+    delay_cnt_done <= '1' when delay_cnt_reg = 0 else '0';
+    
+    sync_o   <= valid_i and ready;
+    active_o <= not env_cnt_done;
+    ready    <= env_cnt_done and delay_cnt_done;
+    ready_o  <= ready;
+    addr_o   <= std_logic_vector(env_cnt_reg(31 downto 32-ENV_LUT_ADDR_BITS));
 
 end architecture rtl;
